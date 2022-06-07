3403b1bf720298020ba61144c90631562957a5ae
Suraj Mandal <surajxuraj@gmail.com>
2022-06-07 11:15:11 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  final String name = "Suraj Mandal";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //it's a widget just like in html consists of head,body
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ), //head
+      body: Center(
+        child: Container(
+          child: Text(
+              "Hello it's me $name"), //$ is used for string manipulation with some data
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}

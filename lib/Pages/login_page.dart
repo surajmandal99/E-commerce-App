9265ac895181293bb957f86c636420d0d55ca172
Suraj Mandal <surajxuraj@gmail.com>
2022-06-09 22:59:04 +0530

    day 4 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -6,16 +6,59 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
+        child: Column(
+      children: [
+        Image.asset(
+          "assets/images/login_image.png",
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 20, //gap
+        ),
+        Text(
+          "Welcome",
           style: TextStyle(
-            fontSize: 20,
-            color: Colors.lightGreen,
+            fontSize: 25,
             fontWeight: FontWeight.bold,
           ),
         ),
-      ),
-    );
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  //for decorating the input text
+                  hintText: "Enter Username",
+                  labelText: "Username",
+                ),
+              ),
+              TextFormField(
+                obscureText: true, //for hiding the pss
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+              ),
+              SizedBox(
+                // for giving the space b/w img and txt
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  //performs the action on press a/c the user defn conditon
+                  print("Hello suraj");
+                },
+                child: Text("Login"),
+                style: TextButton.styleFrom(),
+              )
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
